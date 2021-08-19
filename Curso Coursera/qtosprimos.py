3dd46c720bac74d22a0ed4162060514fd49996c8
rafaolivato <88344941+rafaolivato@users.noreply.github.com>
2021-08-19 09:14:11 -0300

    Add files via upload

---
+++ Curso Coursera/qtosprimos.py
@@ -0,0 +1,24 @@
+def main():
+
+    # leia um numero da sequencia
+    num = int(input("Digite um numero (0 para terminar): "))
+
+    cont = 0  # contador de primos
+    while num != 0:
+        # verifica se num eh primo
+        primo = True
+        i = 2
+        while i < num and primo:
+            if num % i == 0:    # se num eh multiplo de i
+                primo = False   # num nao pode ser primo
+            i += 1
+        if primo:
+            cont += 1
+
+        num = int(input("Digite um numero (0 para terminar): "))
+
+    print ("Achei %d primos na sequencia"%(cont))
+
+#-----------------------------------------------------
+main()
+
