a901002fcc314e6f8cebd969a90a46b670c45461
rafaolivato <88344941+rafaolivato@users.noreply.github.com>
2021-08-23 14:52:39 -0300

    Add files via upload

---
+++ Curso Coursera/retangulo01.py
@@ -0,0 +1,18 @@
+l = int(input("digite a largura: "))
+
+a = int(input("digite a altura: "))
+
+n = 0
+x = 0
+
+while x < a:
+
+    while n < l:
+        print("#", end="")
+
+        n += 1
+    print()
+    x+=1
+    n=0
+
+
